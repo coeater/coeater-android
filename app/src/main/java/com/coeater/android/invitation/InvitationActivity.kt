65a2a237146e50fc5075cd30a6f153403c416ce9
sesang06 <sesang06@naver.com>
2020-10-29 18:11:27 +0900

    카카오링크 적용

--- app/src/main/java/com/coeater/android/invitation/InvitationActivity.kt
+++ app/src/main/java/com/coeater/android/invitation/InvitationActivity.kt
@@ -3,7 +3,7 @@ package com.coeater.android.invitation
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.View\
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -17,9 +17,17 @@ import com.coeater.android.model.RoomResponse
 import com.coeater.android.webrtc.CallActivity
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
+import com.coeater.android.model.RoomResponse
+import com.kakao.sdk.link.LinkClient
+import com.kakao.sdk.template.model.*
 import kotlinx.android.synthetic.main.fragment_oneonone_code.*
 
 class InvitationActivity : AppCompatActivity() {
+
+
+    companion object {
+        const val TAG = "InvitationActivity"
+    }
 
     private val viewModelFactory by lazy {
         InvitationViewModelFactory(
@@ -48,7 +56,7 @@ class InvitationActivity : AppCompatActivity() {
         tv_state.text = "Share"
         et_code_number.visibility = View.GONE
         btn_share_or_join.setOnClickListener {
-
+            share()
         }
         viewModel.roomCreateSuccess.observe(this, Observer<RoomResponse> {
             tv_code_number.text = it.room_code
@@ -71,8 +79,54 @@ class InvitationActivity : AppCompatActivity() {
         })
     }
 
-    override fun onStart() {
-        super.onStart()
+    fun share() {
+        val defaultFeed = FeedTemplate(
+            content = Content(
+                title = "딸기 치즈 케익",
+                description = "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
+                imageUrl = "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+                link = Link(
+                    webUrl = "https://developers.kakao.com",
+                    mobileWebUrl = "https://developers.kakao.com"
+                )
+            ),
+            social = Social(
+                likeCount = 286,
+                commentCount = 45,
+                sharedCount = 845
+            ),
+            buttons = listOf(
+                Button(
+                    "웹으로 보기",
+                    Link(
+                        webUrl = "https://developers.kakao.com",
+                        mobileWebUrl = "https://developers.kakao.com"
+                    )
+                ),
+                Button(
+                    "앱으로 보기",
+                    Link(
+                        androidExecParams = mapOf("key1" to "value1", "key2" to "value2"),
+                        iosExecParams = mapOf("key1" to "value1", "key2" to "value2")
+                    )
+                )
+            )
+        )
+
+        // 피드 메시지 보내기
+        LinkClient.instance.defaultTemplate(this, defaultFeed) { linkResult, error ->
+            if (error != null) {
+                Log.e(TAG, "카카오링크 보내기 실패", error)
+            }
+            else if (linkResult != null) {
+                Log.d(TAG, "카카오링크 보내기 성공 ${linkResult.intent}")
+                startActivity(linkResult.intent)
+
+                // 카카오링크 보내기에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+                Log.w(TAG, "Warning Msg: ${linkResult.warningMsg}")
+                Log.w(TAG, "Argument Msg: ${linkResult.argumentMsg}")
+            }
+        }
     }
 
     private fun checkPermission(url: String) {
