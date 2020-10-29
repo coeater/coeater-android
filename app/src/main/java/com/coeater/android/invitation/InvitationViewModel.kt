48348a82c81651f8513bd2d001a7a54ff8f15d09
kiwoong kim <helloing0119@naver.com>
2020-10-29 18:13:57 +0900

    Call Activity로 전환 러프하게 구현

--- app/src/main/java/com/coeater/android/invitation/InvitationViewModel.kt
+++ app/src/main/java/com/coeater/android/invitation/InvitationViewModel.kt
@@ -6,12 +6,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.coeater.android.api.MatchApi
 import com.coeater.android.api.UserManageProvider
+import com.coeater.android.model.AcceptedState
 import com.coeater.android.model.FriendsInfo
 import com.coeater.android.model.RoomResponse
 import com.coeater.android.model.HTTPResult
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
+import java.net.HttpRetryException
 
 class InvitationViewModel(
     private val api: MatchApi,
@@ -22,9 +25,9 @@ class InvitationViewModel(
         MutableLiveData<RoomResponse>()
     }
 
-    fun onCreate() {
+    fun onCreate(id: Int?) {
         viewModelScope.launch(Dispatchers.IO) {
-            val response = createRoom()
+            val response = createRoom(id)
             when (response) {
                 is HTTPResult.Success<RoomResponse> -> {
                     roomCreateSuccess.postValue(response.data)
@@ -34,7 +37,44 @@ class InvitationViewModel(
                     //TODO
                 }
             }
+        }
+    }
 
+    fun onStart(id: Int) {
+        var trigger: Boolean = true
+        viewModelScope.launch(Dispatchers.IO) {
+            while (trigger == null) {
+                delay(1000)
+                val response = getRoom(id)
+                when (response) {
+                    is HTTPResult.Success<RoomResponse> -> {
+                        val accepted: AcceptedState = response.data.accepted
+                        if (accepted != AcceptedState.NOTCHECK) {
+                            trigger = false
+                            roomCreateSuccess.postValue(response.data)
+                        }
+                    }
+
+                    is Error -> {
+                        //TODO
+                    }
+                }
+            }
+        }
+    }
+
+    fun onAccept(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = acceptInvitation(id)
+            when (response) {
+                is HTTPResult.Success<RoomResponse> -> {
+                    roomCreateSuccess.postValue(response.data)
+                }
+                is Error -> {
+
+                    //TODO
+                }
+            }
         }
     }
 
@@ -50,4 +90,32 @@ class InvitationViewModel(
             HTTPResult.Error(e)
         }
     }
+
+    private suspend fun getRoom(id: Int): HTTPResult<RoomResponse> {
+        return try {
+            val response = api.getRoom(id)
+            HTTPResult.Success(response)
+        } catch (e: HttpException) {
+            HTTPResult.Error(e)
+            // 룸이 존재하지 않는 것
+        } catch (e: Exception) {
+            // 네트워크가 불안정함
+            HTTPResult.Error(e)
+        }
+    }
+
+    private suspend fun acceptInvitation(id: Int): HTTPResult<RoomResponse> {
+        return try {
+            val response = api.acceptInvitation(id)
+            HTTPResult.Success(response)
+        } catch (e: HttpException) {
+            HTTPResult.Error(e)
+            // 룸이 존재하지 않는 것
+        } catch (e: Exception) {
+            // 네트워크가 불안정함
+            HTTPResult.Error(e)
+        }
+    }
+
+
 }
