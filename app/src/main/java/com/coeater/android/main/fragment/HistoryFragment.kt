92bc2415228d24a76ea08c87271108061ecf6b8b
dlrlgk4506 <dlrlgk4506@naver.com>
2020-11-17 23:17:27 +0900

    history 검색 기간 설정 구현

--- app/src/main/java/com/coeater/android/main/fragment/HistoryFragment.kt
+++ app/src/main/java/com/coeater/android/main/fragment/HistoryFragment.kt
@@ -1,31 +1,28 @@
 package com.coeater.android.main.fragment
 
+import android.app.DatePickerDialog
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.coeater.android.R
-import com.coeater.android.api.provideHistoryApi
 import com.coeater.android.history.HistoryAdapter
 import com.coeater.android.history.HistoryViewModel
-import com.coeater.android.history.HistoryViewModelFactory
-import com.coeater.android.main.MainActivity
-import com.coeater.android.model.DateTime
 import kotlinx.android.synthetic.main.fragment_history.*
 import java.util.*
 
 class HistoryFragment : Fragment() {
 
     val viewModel : HistoryViewModel by activityViewModels()
-
+    enum class EditPeriod {
+        FROM, TO
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -47,14 +44,40 @@ class HistoryFragment : Fragment() {
                 layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
             }
         })
+        tv_period_from.setOnClickListener {
+            openDatePickerDialog(viewModel.fromDate, EditPeriod.FROM)
+        }
+        tv_period_to.setOnClickListener {
+            openDatePickerDialog(viewModel.toDate, EditPeriod.TO)
+        }
     }
 
-    private fun setPeriod() {
-        val to = Date()
+    private fun openDatePickerDialog(date: Date, type: EditPeriod) {
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        val datePickerListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            when(type) {
+                EditPeriod.FROM -> setPeriod(calendar.time, viewModel.toDate)
+                EditPeriod.TO -> setPeriod(viewModel.fromDate, calendar.time)
+            }
+        }
+
+        val dpd = DatePickerDialog(requireContext(), R.style.MySpinnerDatePickerStyle, datePickerListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+        when(type) {
+            EditPeriod.FROM -> dpd.datePicker.maxDate = viewModel.toDate.time
+            EditPeriod.TO -> dpd.datePicker.minDate = viewModel.fromDate.time
+        }
+        dpd.show()
+    }
+
+    private fun setPeriod(from: Date = Date(Date().year, Date().month-1, Date().date), to: Date = Date()) {
         viewModel.toDate = to
-        val from = Date(to.year, to.month-1, to.date)
         viewModel.fromDate = from
         val sdf = SimpleDateFormat("yy.MM.dd")
-        tv_period.text = "${sdf.format(from)} ~ ${sdf.format(to)}"
+        tv_period_from.text = sdf.format(from)
+        tv_period_to.text = sdf.format(to)
     }
 }
